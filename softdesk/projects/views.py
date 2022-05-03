d38c46d4a55e6e6db20ede1105d0f387fe071834
Papiexx <boutdepapier27@outlook.fr>
2022-05-03 16:27:54 +0200

    Adding CRUD project endpoints

--- softdesk/projects/views.py
+++ softdesk/projects/views.py
@@ -1,12 +1,42 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import mixins, generics
 
-from projects.models import Project
-from projects.serializers import ProjectSerializer
+from .models import Project
+from .serializers import ProjectSerializer
 
 
-class ProjectViewset(ModelViewSet):
+class ProjectList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    """
+    GET projects list method + POST method 
+    """
 
+    queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
-    def get_queryset(self):
-        return Project.objects.all()
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, *kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class ProjectDetail(mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  generics.GenericAPIView):
+    """
+    GET project detail method + PUT and DELETE methods
+    """
+    
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
