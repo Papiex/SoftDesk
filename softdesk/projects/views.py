746a13029d5ce9b2a697324d95b6b3470cb1508a
Papiexx <boutdepapier27@outlook.fr>
2022-05-15 18:06:56 +0200

    Adding IssueViewSet + admin view

--- softdesk/projects/views.py
+++ softdesk/projects/views.py
@@ -1,17 +1,25 @@
 from rest_framework import viewsets
 from rest_framework.validators import ValidationError
 
-from .models import Project
-from .serializers import ProjectSerializer
+from .models import Issue, Project
+from .serializers import ProjectSerializer, IssueSerializer
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
-    """
-    """
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     
     def perform_create(self, serializer):
         if not self.request.POST["type"]:
             raise ValidationError("You need to specify a type !")
+        serializer.save()
+
+
+class IssueViewSet(viewsets.ModelViewSet):
+    serializer_class = IssueSerializer
+
+    def get_queryset(self):
+        return Issue.objects.filter(project=self.kwargs['project_pk'])
+
+    def perform_create(self, serializer):
         serializer.save()
