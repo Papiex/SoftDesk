ccbc9fb7eddbde4a09810a6bfa40379df7368a06
Papiexx <boutdepapier27@outlook.fr>
2022-05-26 16:42:52 +0200

    adding register serializer + view

--- softdesk/authentication/serializers.py
+++ softdesk/authentication/serializers.py
@@ -1,4 +1,6 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField, EmailField
+from rest_framework.validators import UniqueValidator, ValidationError
+from django.contrib.auth.password_validation import validate_password
 
 from .models import User
 
@@ -6,3 +8,27 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username']
+
+
+class RegisterSerializer(ModelSerializer):
+    email = EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+        )
+
+    password = CharField(write_only=True, required=True, validators=[validate_password])
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'first_name', 'last_name')
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True}
+        }
+
+        def create(self, validated_data):
+            user = User.objects.create_user(validated_data['email'], validated_data['password'])
+            user.first_name = validated_data['first_name']
+            user.last_name = validated_data['last_name']
+            user.save()
+            return user
--- softdesk/authentication/views.py
+++ softdesk/authentication/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .models import User
+from .serializers import RegisterSerializer
 
-# Create your views here.
+from rest_framework.generics import CreateAPIView
+
+
+class RegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
