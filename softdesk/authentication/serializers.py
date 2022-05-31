13f05cd71a40c67bbe15b6970e57521770168000
Papiexx <boutdepapier27@outlook.fr>
2022-05-31 11:21:17 +0200

    minor changes

--- softdesk/authentication/serializers.py
+++ softdesk/authentication/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer, CharField, EmailField
-from rest_framework.validators import UniqueValidator, ValidationError
+from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 
 from .models import User
