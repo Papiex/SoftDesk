9a0704ad2f2f0a8227d6f64e3c70b5260c759d75
Papiexx <boutdepapier27@outlook.fr>
2022-06-10 09:24:43 +0200

    minor changes

--- softdesk/authentication/models.py
+++ softdesk/authentication/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from rest_framework.validators import UniqueValidator
+
 from django.db import models
 
 
 class User(AbstractUser):
-    pass
+
+    first_name = models.CharField(max_length=32, blank=False)
+    last_name = models.CharField(max_length=32, blank=False)
