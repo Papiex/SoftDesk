cc8760fe3aaa204e39f503ca97fdcd45ffd7427c
Papiexx <boutdepapier27@outlook.fr>
2022-06-10 09:27:05 +0200

    Register view

--- softdesk/authentication/views.py
+++ softdesk/authentication/views.py
@@ -1,9 +1,21 @@
-from .models import User
-from .serializers import RegisterSerializer
+from .serializers import UserSerializer
 
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 
-class RegisterView(CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
+class RegisterView(GenericAPIView):
+
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs) -> Response:
+        """Create user and return data entered"""
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
+        return Response({'detail': 'User created !'})
+    
+
