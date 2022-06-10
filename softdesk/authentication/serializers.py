300383f91efd026454b508a8052b8067b247331a
Papiexx <boutdepapier27@outlook.fr>
2022-06-10 09:26:00 +0200

    overwrite create user method

--- softdesk/authentication/serializers.py
+++ softdesk/authentication/serializers.py
@@ -1,34 +1,26 @@
-from rest_framework.serializers import ModelSerializer, CharField, EmailField
-from rest_framework.validators import UniqueValidator
+from rest_framework.serializers import ModelSerializer
 from django.contrib.auth.password_validation import validate_password
 
 from .models import User
 
+
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username']
-
-
-class RegisterSerializer(ModelSerializer):
-    email = EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]
-        )
-
-    password = CharField(write_only=True, required=True, validators=[validate_password])
-
-    class Meta:
-        model = User
-        fields = ('email', 'password', 'first_name', 'last_name')
+        fields = ['first_name','last_name', 'username', 'email', 'password']
         extra_kwargs = {
             'first_name': {'required': True},
-            'last_name': {'required': True}
+            'last_name': {'required': True},
         }
 
-        def create(self, validated_data):
-            user = User.objects.create_user(validated_data['email'], validated_data['password'])
-            user.first_name = validated_data['first_name']
-            user.last_name = validated_data['last_name']
-            user.save()
-            return user
+
+    def create(self, validated_data) -> User:
+        """
+        Get the user with post validated data
+        set user.is_active to true and save
+        """
+        user = self.Meta.model(**validated_data)
+        user.set_password(validated_data['password'])
+        user.is_active = True
+        user.save()
+        return user
