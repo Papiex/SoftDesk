bd66e8684cbc3e54c5cd52b78e3ccd177e986949
Papiexx <boutdepapier27@outlook.fr>
2022-06-12 12:33:14 +0200

    adding typehints

--- softdesk/projects/views.py
+++ softdesk/projects/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.db.models.query import QuerySet
 
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
@@ -12,6 +13,10 @@ class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self) -> QuerySet[Project]:
+        """return projects of the connected user"""
+        return Project.objects.filter(author_user_id=self.request.user.pk)
     
     def perform_create(self, serializer):
         if not self.request.POST["type"]:
@@ -23,7 +28,7 @@ class IssueViewSet(viewsets.ModelViewSet):
     serializer_class = IssueSerializer
     permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet[Issue]:
         queryset = Issue.objects.filter(project=self.kwargs['project_pk'])
         if get_object_or_404(queryset):
             return queryset
@@ -38,7 +43,7 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet[Comment]:
         queryset = Comment.objects.filter(issue=self.kwargs['issue_pk'])
         if get_object_or_404(queryset):
             return queryset
