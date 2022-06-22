f7305835fc880153ee5e0beef6219f1d0dd4744b
Papiexx <boutdepapier27@outlook.fr>
2022-06-22 18:47:37 +0200

    no modification ?

--- softdesk/projects/models.py
+++ softdesk/projects/models.py
@@ -22,8 +22,8 @@ class Contributor(models.Model):
     """Model defining a contributor"""
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
     project = models.ForeignKey(to=Project, related_name="project_contributor", on_delete=models.CASCADE, blank=True, null=True)
-    permission = models.CharField(max_length=16, choices=[(permission.name, permission.value) for permission in enums.ProjectPermission], default="All")
-    role = models.CharField(max_length=16, choices=[(role.name, role.value) for role in enums.ProjectRole], default="Author")
+    permission = models.CharField(max_length=16, choices=[(permission.name, permission.value) for permission in enums.ProjectPermission], default="ALL")
+    role = models.CharField(max_length=16, choices=[(role.name, role.value) for role in enums.ProjectRole], default="AUTHOR")
     def __str__(self) -> str:
         return f"Contributor: {str(self.user)}"
 
