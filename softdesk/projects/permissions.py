8ee7a2912a3554b27592bf4e8290339d1dd67a9b
Papiexx <boutdepapier27@outlook.fr>
2022-07-05 14:15:35 +0200

    add permission denied message + has_permission method for project

--- softdesk/projects/permissions.py
+++ softdesk/projects/permissions.py
@@ -1,59 +1,40 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework import permissions
 
 from .models import Project
 
 
-class AuthorOrContributorProject(permissions.BasePermission):
+class AuthorOrReadOnly(permissions.BasePermission):
     """
     Author can update and destroy
     Contributor can retrieve and list projects
     """
 
-    def has_object_permission(self, request, view, obj) -> bool:
-        
-        if view.action in ['list', 'retrieve']:
-            return request.user in obj.contributors.all()
-        elif view.action in ['update', 'partial_update', 'destroy']:
-            return request.user == obj.author_user_id
+    message = "You don't have the permission to do this !"
 
+    def has_permission(self, request, view):
+        if view.kwargs.get('project_pk') is not None:
+            project = get_object_or_404(Project, id=view.kwargs.get('project_pk'))
+            return request.user in project.contributors.all()
+        return True
 
-class IssuePermission(permissions.BasePermission):
-    """
-    Author of issue can update and destroy
-    Contributor can create, retrieve and list issues
-    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user == obj.author_user_id
 
-    def has_object_permission(self, request, view, obj) -> bool:
-        
-        if view.action in ['list', 'retrieve', 'create']:
-            return request.user in obj.project.contributors.all()
-        elif view.action in ['update', 'partial_update', 'destroy']:
-            return request.user == obj.author_user_id
-
-
-class CommentPermission(permissions.BasePermission):
-    """
-    Author of comment can update and destroy
-    Contributor can create, retrieve and list comment
-    """
-
-    def has_object_permission(self, request, view, obj) -> bool:
-        
-        if view.action in ['list', 'retrieve', 'create']:
-            return request.user in obj.issue.project.contributors.all()
-        elif view.action in ['update', 'partial_update', 'destroy']:
-            return request.user == obj.author_user_id
 
 class ContributorPermission(permissions.BasePermission):
     """
     Author of project can add and destroy contributor
     Contributor can list contributor
     """
+    message = "You don't have the permission to do this !"
 
     def has_permission(self, request, view) -> bool:
-
         project = Project.objects.filter(id=view.kwargs['project_pk']).first()
         if view.action in ['list', 'retrieve']:
             return request.user in project.contributors.all()
+
         elif view.action in ['create', 'update', 'partial_update', 'destroy']:
             return request.user == project.author_user_id
